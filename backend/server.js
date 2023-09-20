8b7358da0a59bd1bddf1d4a0ea21d26742422f4c
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-09-20 21:43:44 +0600

    clicking on the tablerow will make the car location center

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,13 @@ const wss = new WebSocket.Server({ server });
 var markerData = [
   { id: 1, lat: 23.8103, lng: 90.4125, distance: 0, status: "Moving" },
   { id: 2, lat: 23.8103, lng: 90.4125, distance: 0, status: "Moving" },
-  { id: 3, lat: 23.8103, lng: 90.4125, distance: 0, status: "idle" },
+  { id: 3, lat: 23.8103, lng: 90.4125, distance: 0, status: "Idle" },
   { id: 4, lat: 23.8759, lng: 90.3904, distance: 0, status: "Moving" },
-  { id: 5, lat: 23.7465, lng: 90.376, distance: 0, status: "idle" },
+  { id: 5, lat: 23.7465, lng: 90.376, distance: 0, status: "Idle" },
 ];
 
 function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Radius of the Earth in kilometers
+  const R = 6371;
   const dLat = (lat2 - lat1) * (Math.PI / 180);
   const dLon = (lon2 - lon1) * (Math.PI / 180);
   const a =
@@ -28,7 +28,7 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
       Math.sin(dLon / 2) *
       Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c; // Distance in kilometers
+  const distance = R * c;
   return distance;
 }
 
@@ -53,7 +53,7 @@ wss.on("connection", (ws) => {
           ...marker,
           lat: newLat,
           lng: newLng,
-          distance: marker.distance + distanceMoved, // Update the distance
+          distance: marker.distance + distanceMoved, 
         };
       } else {
         return marker;
